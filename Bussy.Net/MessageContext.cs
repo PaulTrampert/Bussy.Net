88a433eea5847e85d675d3a389cdb578307b203d
Paul Trampert <paul.trampert@ptrampert.com>
2026-03-20 14:47:47 -0400

    Doc message context

--- Bussy.Net/MessageContext.cs
+++ Bussy.Net/MessageContext.cs
@@ -2,6 +2,16 @@ using System;
 
 namespace Bussy.Net;
 
+/// <summary>
+/// Context object that is passed to handlers when a message is received. Contains metadata about the message, such as the topic and broker it was received from, as well as the message itself.
+/// </summary>
+/// <param name="Id">Message Id</param>
+/// <param name="SendTime">Timestamp when the message was sent (UTC)</param>
+/// <param name="ReceiveTime">Timestamp when the message was received (UTC)</param>
+/// <param name="Topic">The topic the message was published to</param>
+/// <param name="Broker">The broker the message was received from</param>
+/// <param name="Message">The message</param>
+/// <typeparam name="TMessage">The message type</typeparam>
 public record MessageContext<TMessage>(
     Guid Id,
     DateTime SendTime,
