e86fc453248639e04fbfec0732fbc04738c6c7c8
Paul Trampert <paul.trampert@ptrampert.com>
2026-03-29 18:40:17 -0400

    Implement InMemoryTransport and InMemoryTransportSubscription

--- Bussy.Net.Transports.InMemory/InMemoryTransport.cs
+++ Bussy.Net.Transports.InMemory/InMemoryTransport.cs
@@ -1,24 +1,44 @@
-﻿using Bussy.Net.Transport;
+﻿using System.Collections.Concurrent;
+using System.Threading.Channels;
+using Bussy.Net.Transport;
+using Microsoft.Extensions.Logging;
 
 namespace Bussy.Net.Transports.InMemory;
 
-public class InMemoryTransport : ITransport
+public class InMemoryTransport(ILoggerFactory loggerFactory) : ITransport
 {
-    public Task SendAsync(OutboundMessage message, CancellationToken cancellationToken = default)
+    private readonly ConcurrentDictionary<string, InMemoryTransportSubscription> _subscriptions = new();
+    
+    public string Name => "InMemory";
+    public TransportCapability Capabilities => TransportCapability.None;
+
+    public async Task SendAsync(OutboundMessage message, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        if (!_subscriptions.TryGetValue(message.Topic, out var subscription))
+        {
+            return;
+        }
+        var inboundMessage = new InboundMessage(
+            message.Body,
+            message.Topic,
+            message.Broker,
+            message.Headers,
+            message.MessageId,
+            message.SentAtUtc,
+            DateTimeOffset.UtcNow,
+            0);
+        
+        await subscription.EnqueueAsync(inboundMessage, cancellationToken);
     }
 
-    public Task SendBatchAsync(IReadOnlyCollection<OutboundMessage> messages, CancellationToken cancellationToken = default)
+    public async Task SendBatchAsync(IReadOnlyCollection<OutboundMessage> messages, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        await Task.WhenAll(messages.Select(m => SendAsync(m, cancellationToken)));
     }
-
+    
     public Task<ITransportSubscription> SubscribeAsync(string topic, Func<InboundMessage, CancellationToken, Task<AckAction>> onMessage, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var subscription = _subscriptions.GetOrAdd(topic, key => new InMemoryTransportSubscription($"{Name}_{key}", onMessage, loggerFactory.CreateLogger<InMemoryTransportSubscription>(), cancellationToken));
+        return Task.FromResult<ITransportSubscription>(subscription);
     }
-
-    public string Name { get; }
-    public TransportCapability Capabilities { get; }
 }
--- Bussy.Net.Transports.InMemory/InMemoryTransportSubscription.cs
+++ Bussy.Net.Transports.InMemory/InMemoryTransportSubscription.cs
@@ -1,18 +1,73 @@
+using System.Threading.Channels;
 using Bussy.Net.Transport;
+using Microsoft.Extensions.Logging;
 
 namespace Bussy.Net.Transports.InMemory;
 
 public class InMemoryTransportSubscription : ITransportSubscription
 {
-    public string Name { get; init; }
+    private readonly Channel<InboundMessage> _messages = Channel.CreateUnbounded<InboundMessage>();
+
+    private readonly ILogger<InMemoryTransportSubscription> _logger;
+    
+    private readonly Task _processTask;
+    
+    internal InMemoryTransportSubscription(
+        string name,
+        Func<InboundMessage, CancellationToken, Task<AckAction>> processMessage,
+        ILogger<InMemoryTransportSubscription> logger, 
+        CancellationToken cancellationToken
+    )
+    {
+        Name = name;
+        _logger = logger;
+        _processTask = ProcessAsync(processMessage, cancellationToken);
+    }
+    
+    public string Name { get; }
+
+    internal async Task EnqueueAsync(InboundMessage message, CancellationToken cancellationToken)
+    {
+        await _messages.Writer.WriteAsync(message, cancellationToken);
+    }
+    
+    private async Task ProcessAsync(Func<InboundMessage, CancellationToken, Task<AckAction>> processMessage, CancellationToken cancellationToken)
+    {
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            try
+            {
+                var message = await _messages.Reader.ReadAsync(cancellationToken);
+                var result = await processMessage(message, cancellationToken);
+                if (result == AckAction.Retry)
+                {
+                    await _messages.Writer.WriteAsync(message, cancellationToken);
+                }
+
+                if (result == AckAction.DeadLetter)
+                {
+                    _logger.LogError("Dead letter message: {@Message}", message);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error processing message in subscription {SubscriptionName}", Name);
+            }
+        }
+    }
     
     public void Dispose()
     {
-        // TODO release managed resources here
+        _messages.Writer.Complete();
     }
 
     public async ValueTask DisposeAsync()
     {
-        // TODO release managed resources here
+        _messages.Writer.Complete();
+        await  _processTask;
     }
 }
