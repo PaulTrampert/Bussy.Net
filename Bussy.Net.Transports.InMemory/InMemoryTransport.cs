aad19b25d877d9263c6499d13d93037e9effacce
Paul Trampert <paul.trampert@ptrampert.com>
2026-03-29 14:54:39 -0400

    Stub out the transport

---
+++ Bussy.Net.Transports.InMemory/InMemoryTransport.cs
@@ -0,0 +1,24 @@
+﻿using Bussy.Net.Transport;
+
+namespace Bussy.Net.Transports.InMemory;
+
+public class InMemoryTransport : ITransport
+{
+    public Task SendAsync(OutboundMessage message, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SendBatchAsync(IReadOnlyCollection<OutboundMessage> messages, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ITransportSubscription> SubscribeAsync(string topic, Func<InboundMessage, CancellationToken, Task<AckAction>> onMessage, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Name { get; }
+    public TransportCapability Capabilities { get; }
+}
